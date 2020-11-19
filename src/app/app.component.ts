4aee3e222cb6846e650707b197e7fcb9e088b842
Ligre <ligreman@users.noreply.github.com>
2020-11-19 23:31:27 +0100

    Added vis.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { DataSet, Network } from 'vis-network/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'king';
 
   constructor(translate: TranslateService) {
@@ -16,4 +17,36 @@ export class AppComponent {
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('en');
   }
+
+  ngOnInit(): void {
+    // create an array with nodes
+    const nodes = new DataSet([
+      {id: 1, label: 'Node 1'},
+      {id: 2, label: 'Node 2'},
+      {id: 3, label: 'Node 3'},
+      {id: 4, label: 'Node 4'},
+      {id: 5, label: 'Node 5'}
+    ]);
+
+    // create an array with edges
+    // @ts-ignore
+    const edges = new DataSet([
+      {from: 1, to: 3},
+      {from: 1, to: 2},
+      {from: 2, to: 4},
+      {from: 2, to: 5},
+      {from: 3, to: 3}
+    ]);
+
+    // create a network
+    const container = document.getElementById('mynetwork');
+    const data = {
+      nodes: nodes,
+      edges: edges
+    };
+    const options = {};
+    const network = new Network(container, data, options);
+  }
+
+
 }
